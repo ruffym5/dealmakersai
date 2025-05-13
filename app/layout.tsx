ca4075f19d0827fdce80c648fe8fbc7dacb3dacc
ooe <onur-oezcelik@gmx.de>
2025-05-13 18:21:18 +0200

    Hubspot Script

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from 'next/script'
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,6 +30,12 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        {/* HubSpot Tracking */}
+        <Script
+          id="hs-script-loader"
+          strategy="afterInteractive"
+          src="//js-eu1.hs-scripts.com/146200295.js"
+        />
       </body>
     </html>
   );
